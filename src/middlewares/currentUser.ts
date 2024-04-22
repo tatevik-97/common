ced277f4881df8d1c905b426f166267b5a9d6be0
tatevik-97 <tatevike9@gmail.com>
2024-04-22 14:26:04 +0400

    publish

--- src/middlewares/currentUser.ts
+++ src/middlewares/currentUser.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { UserPayload } from "../types/types";
+import {NotAuthorizedError} from "../../build";
 
 export const currentUser = async (
   req: Request,
@@ -11,16 +12,15 @@ export const currentUser = async (
     !req.headers.authorization ||
     !req.headers.authorization.startsWith("Bearer ")
   ) {
-    return next();
-  }
+throw new NotAuthorizedError()  }
 
   try {
     const token = req.headers.authorization.split(" ")[1];
     const payload = jwt.verify(token, process.env.JWT_KEY as string);
 
     if (!payload) {
-      return next();
-    }
+      throw new NotAuthorizedError()
+  }
 
     req.currentUser = payload as UserPayload;
   } catch (err) {
